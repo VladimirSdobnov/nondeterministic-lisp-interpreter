eb06a87eb902551c7164b740b359434a09ad25c8
Сдобнов Владимир Александрович <vladimir.sdobnov@omnichannel.ru>
2026-05-14 16:21:57 +0300

    Add Parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,47 @@
 module Main where
 
+import Control.Exception
 import Parser
 
+runTest :: String -> IO ()
+runTest input = do
+
+    putStrLn "================================="
+    putStrLn ("Input: " ++ input)
+
+    result <- try (evaluateParser input)
+        :: IO (Either SomeException ())
+
+    case result of
+
+        Left ex -> do
+            putStrLn "Error:"
+            print ex
+
+        Right _ ->
+            putStrLn "Success"
+
+-- Выполняет tokenize + parse
+evaluateParser :: String -> IO ()
+evaluateParser input = do
+
+    let tokens = tokenize input
+
+    putStrLn "Tokens:"
+    print tokens
+
+    putStrLn "AST:"
+    print (parse tokens)
+
 main :: IO ()
-main = print (tokenize "(+ (+ 1 2) (+ 32 4))")
+main = do
+
+    runTest "(+ 1 (* 2 3))"
+
+    runTest "(+ 1 (* 2 3))))"
+
+    runTest "(+ 1 (* 2 3)"
+
+    runTest ")"
+
+    runTest "#x"
